39e11cbfca8732c26cfecfda31cd5962840c952d
João <GabrielAlvees616@gmail.com>
2025-05-24 22:32:07 -0300

    Criado o Command e o CommandHandler de Usuario no Contexto de Usuario

---
+++ src/Usuario/DOS.Usuario.Application/Commands/UsuarioCriadoCommand.cs
@@ -0,0 +1,24 @@
+﻿using DOS.Core.Mediator.Commands;
+using DOS.Usuario.Domain.Enums;
+
+namespace DOS.Usuario.Application.Commands
+{
+    public class UsuarioCriadoCommand : ICommand<Guid>
+    {
+        public string Nome { get; private set; }
+        public string CPF { get; private set; }
+        public string Telefone { get; private set; }
+        public string Email { get; private set; }
+        public TipoSanguineo TipoSanguineo { get; private set; }
+
+        public UsuarioCriadoCommand(string nome,string email,
+            string cpf, string telefone, TipoSanguineo tipoSanguineo)
+        {
+            Nome = nome;
+            Email = email;
+            CPF = cpf;
+            Telefone = telefone;
+            TipoSanguineo = tipoSanguineo;
+        }
+    }
+}
---
+++ src/Usuario/DOS.Usuario.Application/CommandsHandlers/UsuarioCriadoCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using DOS.Core.Mediator.Commands;
+using DOS.Usuario.Application.Commands;
+using DOS.Usuario.Domain;
+
+namespace DOS.Usuario.Application.CommandsHandlers
+{
+    public class UsuarioCriadoCommandHandler : ICommandHandler<UsuarioCriadoCommand, Guid>
+    {
+        private readonly IUsuarioRepository _usuarioRepository;
+
+        public UsuarioCriadoCommandHandler(IUsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+        public async Task<Guid> HandleAsync(UsuarioCriadoCommand command)
+        {
+            var usuario = new User(command.Nome,command.Email, command.CPF, command.Telefone, command.TipoSanguineo);
+            await _usuarioRepository.Adcionar(usuario);
+            var sucesso = await _usuarioRepository.UnitOfWork.Commit();
+            if (!sucesso)
+            {
+                throw new Exception("Erro ao salvar o usuário");
+            }
+            return usuario.Id;
+        }
+    }
+}
+
