cfcfcc5158d48719607bceaced1dfe77d466eb0c
João <GabrielAlvees616@gmail.com>
2025-05-25 20:20:24 -0300

    Adicionado API de Usuario

---
+++ src/Usuario/DOS.Usuario.API/Controllers/UsuarioController.cs
@@ -0,0 +1,38 @@
+﻿using DOS.Core.Mediator.Commands;
+using DOS.Usuario.Application.Commands;
+using DOS.Usuario.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DOS.Usuario.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioRepository _usuarioRepository;
+        private readonly ICommandDispatcher _commandDispatcher;
+
+        public UsuarioController(IUsuarioRepository usuarioRepository,
+            ICommandDispatcher commandDispatcher)
+        {
+            _usuarioRepository = usuarioRepository;
+            _commandDispatcher = commandDispatcher;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CriarUsuario([FromBody] UsuarioCriadoCommand request)
+        {
+            var email = User.Claims.FirstOrDefault(c => c.Type == "email")?.Value;
+
+            if (string.IsNullOrEmpty(email))
+                return Unauthorized("Email não encontrado no token.");
+
+            request.SetEmail(email); 
+
+            var usuarioId = await _commandDispatcher
+                .DispatchAsync<UsuarioCriadoCommand, Guid>(request);
+
+            return CreatedAtAction(nameof(CriarUsuario), new { id = usuarioId }, usuarioId);
+        }
+    }
+}
---
+++ src/Usuario/DOS.Usuario.Data/Mediator/CommandDispatcher.cs
@@ -0,0 +1,24 @@
+﻿
+using DOS.Core.Mediator.Commands;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DOS.Usuario.Data.Mediator
+{
+    public class CommandDispatcher : ICommandDispatcher
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public CommandDispatcher(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+        public async Task<TResult> DispatchAsync<TCommand, TResult>(TCommand command)
+            where TCommand : ICommand<TResult>
+        {
+            var handler = _serviceProvider.GetService<ICommandHandler<TCommand, TResult>>();
+            if (handler == null)
+                throw new InvalidOperationException($"Handler não encontrado para o comando: {typeof(TCommand).Name}");
+            return await handler.HandleAsync(command);
+        }
+    }
+}
