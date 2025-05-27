d8a36bccc711c75676a56d6f37c55be194d55e72
João <GabrielAlvees616@gmail.com>
2025-05-27 16:28:56 -0300

    Implementado API do contexto de Doação

---
+++ src/Doacao/DOS.Doacao.API/Controllers/DoacaoController.cs
@@ -0,0 +1,35 @@
+﻿using DOS.Core.Mediator.Commands;
+using DOS.Doacao.API.DTOs;
+using DOS.Doacao.Application.Commands;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace DOS.Doacao.API.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class DoacaoController : ControllerBase
+    {
+        private readonly ICommandDispatcher _commandDispatcher;
+
+        public DoacaoController(ICommandDispatcher commandDispatcher)
+        {
+            _commandDispatcher = commandDispatcher;
+        }
+        [HttpPost("agendar")]
+        public async Task<IActionResult> Agendar([FromBody] AgendarDoacaoDTO request)
+        {
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+            if (!Guid.TryParse(userIdClaim.Value, out var userId))
+                return BadRequest("UserId inválido nas claims");
+
+            var command = new AgendarDoacaoCommand(request.AgendaId, request.DataHoraAgendada);
+            command.UserId = userId;
+            var doacaoId = await _commandDispatcher.DispatchAsync<AgendarDoacaoCommand, Guid>(command);
+            return CreatedAtAction(nameof(Agendar), new { id = doacaoId }, doacaoId);
+        }
+    }
+}
+
