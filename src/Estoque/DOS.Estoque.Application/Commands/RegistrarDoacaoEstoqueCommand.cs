a2c3c8ab18292cf42c6372ea4882e8b92d24bcc2
João <GabrielAlvees616@gmail.com>
2025-05-29 15:41:16 -0300

    Adicionado a camada Application do Contexto de Estoque

---
+++ src/Estoque/DOS.Estoque.Application/Commands/RegistrarDoacaoEstoqueCommand.cs
@@ -0,0 +1,15 @@
+﻿using DOS.Core.Mediator.Commands;
+using DOS.Estoque.Domain.Enums;
+namespace DOS.Estoque.Application.Commands
+{
+    public class RegistrarDoacaoEstoqueCommand : ICommand<bool>
+    {
+        public TipoSanguineo TipoSanguineo { get; }
+
+        public RegistrarDoacaoEstoqueCommand(TipoSanguineo tipoSanguineo)
+        {
+            TipoSanguineo = tipoSanguineo;
+        }
+    }
+}
+    
