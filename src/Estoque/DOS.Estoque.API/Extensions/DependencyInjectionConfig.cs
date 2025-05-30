950a11b0d31fe4445a0dafa7c5e98df779cd3905
João <GabrielAlvees616@gmail.com>
2025-05-30 08:18:40 -0300

    Adcionado a camada de API no contexto de Estoque

---
+++ src/Estoque/DOS.Estoque.API/Extensions/DependencyInjectionConfig.cs
@@ -0,0 +1,29 @@
+﻿using DOS.Core.Mediator.Commands;
+using DOS.Estoque.Application.Commands;
+using DOS.Estoque.Application.CommandsHandlers;
+using DOS.Estoque.Data;
+using DOS.Estoque.Data.Mediator;
+using DOS.Estoque.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DOS.Estoque.API.Extensions
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection AddDependencyInjection(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddScoped<IEstoqueRepository, EstoqueRepository>();
+            services.AddScoped<ICommandHandler<RegistrarDoacaoEstoqueCommand, bool>,
+                RegistrarDoacaoEstoqueCommandHandler>();
+            services.AddScoped<ICommandDispatcher, CommandDispatcher>();
+
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<EstoqueContext>(options =>
+                        options.UseSqlServer(connectionString));
+
+            return services;
+        }
+    }
+}
