58dadc63db9e0ad61ecc0dc05978d12a11fca008
João <GabrielAlvees616@gmail.com>
2025-06-05 18:38:32 -0300

    Criado o evento DoacaoAgendadaEvent e aplicado o mesmo no CommandHandler após a criação de Doação

---
+++ src/Doacao/DOS.Doacao.Application/Eventos/DoacaoAgendadaEvent.cs
@@ -0,0 +1,22 @@
+﻿using DOS.Core.DomainObjects;
+
+namespace DOS.Doacao.Application.Eventos
+{
+    public class DoacaoAgendadaEvent : IDomainEvent
+    {
+        public Guid Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public Guid DoacaoId { get; private set; }
+        public DateTime DataHoraAgendada { get; private set; }
+        public DateTime OcurreuEm => DateTime.UtcNow;
+        public DoacaoAgendadaEvent(string nome, string email, Guid doacaoId, DateTime dataHoraAgendada)
+        {
+            Id = Guid.NewGuid();
+            Nome = nome;
+            Email = email;
+            DoacaoId = doacaoId;
+            DataHoraAgendada = dataHoraAgendada;
+        }
+    }
+}
