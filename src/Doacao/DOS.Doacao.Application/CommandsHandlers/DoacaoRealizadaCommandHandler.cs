6b34493e21c4abd9047468bff98b9c281d4a6535
João <GabrielAlvees616@gmail.com>
2025-06-10 20:57:57 -0300

    Criado o command e o commandhandler de quando a doação é realizada e tem seu status alterado

---
+++ src/Doacao/DOS.Doacao.Application/CommandsHandlers/DoacaoRealizadaCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using DOS.Core.Mediator.Commands;
+using DOS.Doacao.Application.Commands;
+using DOS.Doacao.Domain;
+
+namespace DOS.Doacao.Application.CommandsHandlers
+{
+    public class DoacaoRealizadaCommandHandler : ICommandHandler<DoacaoRealizadaCommand, bool>  
+    {
+        private readonly IDoacaoRepository _doacaoRepository;
+
+
+        public DoacaoRealizadaCommandHandler(IDoacaoRepository doacaoRepository)
+        {
+            _doacaoRepository = doacaoRepository;
+        
+        }
+        public async Task<bool> HandleAsync(DoacaoRealizadaCommand command)
+        {
+            var doacao = await _doacaoRepository.ObterPorIdAsync(command.DoacaoId);
+            if (doacao == null)
+                throw new Exception("Doação não encontrada.");
+
+            doacao.Iniciar();
+
+            var sucesso = await _doacaoRepository.UnitOfWork.Commit();
+            if (!sucesso)
+                throw new Exception("Erro ao iniciar a doação.");
+
+            return true;
+        }
+    }
+}
