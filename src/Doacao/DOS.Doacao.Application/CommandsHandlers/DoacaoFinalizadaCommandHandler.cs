cada5d97ff1edbf6aa1e5de8cf1c356e4effdea0
João <GabrielAlvees616@gmail.com>
2025-06-11 09:58:20 -0300

    Adicionado command e commandHandler de DoacaoFinalizada

---
+++ src/Doacao/DOS.Doacao.Application/CommandsHandlers/DoacaoFinalizadaCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using DOS.Core.Mediator.Commands;
+using DOS.Doacao.Application.Commands;
+using DOS.Doacao.Domain;
+
+namespace DOS.Doacao.Application.CommandsHandlers
+{
+    public class DoacaoFinalizadaCommandHandler : ICommandHandler<DoacaoFinalizadaCommand,bool>
+    {
+        private readonly IDoacaoRepository _doacaoRepository;
+
+        public DoacaoFinalizadaCommandHandler(IDoacaoRepository repository)
+        {
+            _doacaoRepository = repository;
+        }
+
+        public async Task<bool> HandleAsync(DoacaoFinalizadaCommand command)
+        {
+            var doacao = await _doacaoRepository.ObterPorIdAsync(command.DoacaoId);
+            if (doacao == null)
+            {
+                throw new Exception("Doação não encontrada");
+            }
+            doacao.Finalizar();
+            var sucesso = await _doacaoRepository.UnitOfWork.Commit();
+            if (!sucesso)
+            {
+                throw new Exception("Erro ao finalizar doação");
+            }
+            return true;
+
+            
+        }
+    }
+}
