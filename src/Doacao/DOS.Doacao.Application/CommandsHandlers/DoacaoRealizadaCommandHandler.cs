60298451bdc79b29032308d317e9812ef5edb4e1
João <GabrielAlvees616@gmail.com>
2025-06-10 21:06:54 -0300

    Criado DoacaoRealizadaEvent que notifica o usuário após a doação

--- src/Doacao/DOS.Doacao.Application/CommandsHandlers/DoacaoRealizadaCommandHandler.cs
+++ src/Doacao/DOS.Doacao.Application/CommandsHandlers/DoacaoRealizadaCommandHandler.cs
@@ -1,19 +1,27 @@
-﻿using DOS.Core.Mediator.Commands;
+﻿using DOS.Core.DomainObjects;
+using DOS.Core.Mediator.Commands;
 using DOS.Doacao.Application.Commands;
+using DOS.Doacao.Application.Eventos;
 using DOS.Doacao.Domain;
+using DOS.Usuario.Domain;
 
 namespace DOS.Doacao.Application.CommandsHandlers
 {
     public class DoacaoRealizadaCommandHandler : ICommandHandler<DoacaoRealizadaCommand, bool>  
     {
         private readonly IDoacaoRepository _doacaoRepository;
+        private readonly IUsuarioRepository _usuarioRepository;
+        private readonly IDomainEventDispatcher _domainEventDispatcher;
 
-
-        public DoacaoRealizadaCommandHandler(IDoacaoRepository doacaoRepository)
+        public DoacaoRealizadaCommandHandler(IDoacaoRepository doacaoRepository,
+            IUsuarioRepository usuarioRepository,
+            IDomainEventDispatcher domainEventDispatcher)
         {
             _doacaoRepository = doacaoRepository;
-        
+            _usuarioRepository = usuarioRepository;
+            _domainEventDispatcher = domainEventDispatcher;
         }
+
         public async Task<bool> HandleAsync(DoacaoRealizadaCommand command)
         {
             var doacao = await _doacaoRepository.ObterPorIdAsync(command.DoacaoId);
@@ -25,6 +33,21 @@ namespace DOS.Doacao.Application.CommandsHandlers
             var sucesso = await _doacaoRepository.UnitOfWork.Commit();
             if (!sucesso)
                 throw new Exception("Erro ao iniciar a doação.");
+
+            var usuario = await _usuarioRepository.GetById(doacao.UsuarioId);
+            if (usuario == null)
+                throw new Exception("Usuário da doação não encontrado.");
+
+            await _domainEventDispatcher.DispatchEventsAsync(
+                  new List<IDomainEvent>
+                  {
+                    new DoacaoRealizadaEvent(
+                        doacao.Id,
+                        usuario.Email,
+                        usuario.Nome
+                    )
+                  }
+        );
 
             return true;
         }
---
+++ src/Doacao/DOS.Doacao.Application/Eventos/DoacaoRealizadaEvent.cs
@@ -0,0 +1,20 @@
+﻿
+using DOS.Core.DomainObjects;
+
+namespace DOS.Doacao.Application.Eventos
+{
+    public class DoacaoRealizadaEvent : IDomainEvent
+    {
+        public Guid DoacaoId { get; set; }
+        public string Email { get; set; }
+        public string Nome { get; set; }
+        public DateTime OcurreuEm => DateTime.Now;
+
+        public DoacaoRealizadaEvent(Guid doacaoId, string email, string nome)
+        {
+            DoacaoId = doacaoId;
+            Email = email;
+            Nome = nome;
+        }
+    }
+}
