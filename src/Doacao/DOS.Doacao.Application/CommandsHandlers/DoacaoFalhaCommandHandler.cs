a7467ef7379b6dbb84babbbeb8da5430596c198b
João <GabrielAlvees616@gmail.com>
2025-06-11 11:37:26 -0300

    Criado DoacaoFalhaCommand e implementado no CommandHandler

---
+++ src/Doacao/DOS.Doacao.Application/CommandsHandlers/DoacaoFalhaCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using DOS.Core.Mediator.Commands;
+using DOS.Doacao.Application.Commands;
+using DOS.Doacao.Domain;
+
+namespace DOS.Doacao.Application.CommandsHandlers
+{
+    public class DoacaoFalhaCommandHandler : ICommandHandler<DoacaoFalhaCommand,bool>
+    {
+        private readonly IDoacaoRepository _doacaoRepository;
+
+        public DoacaoFalhaCommandHandler(IDoacaoRepository doacaoRepository)
+        {
+            _doacaoRepository = doacaoRepository;
+        }
+
+        public async Task<bool> HandleAsync(DoacaoFalhaCommand command)
+        {
+            var doacao = await _doacaoRepository.ObterPorIdAsync(command.DoacaoId);
+            if (doacao == null)
+            {
+                throw new Exception("Doação não encontrada");
+            }
+            doacao.MarcarComoFalha();
+            var sucesso = await _doacaoRepository.UnitOfWork.Commit();
+            if (!sucesso)
+            {
+                throw new Exception("Erro em alterar status da doação");
+            }
+            return true;
+        }
+    }
+}
