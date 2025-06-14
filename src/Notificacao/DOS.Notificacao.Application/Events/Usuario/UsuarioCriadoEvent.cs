52ff87c498501cc67901754118304aa64a9b0ce8
João <GabrielAlvees616@gmail.com>
2025-06-14 11:23:57 -0300

    Criado camada de application do contexto notificação e criado evento e eventohandler de UsuarioCriado

---
+++ src/Notificacao/DOS.Notificacao.Application/Events/Usuario/UsuarioCriadoEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace DOS.Notificacao.Application.Events.Usuario
+{
+    public class UsuarioCriadoEvent
+    {
+        public string Nome { get;  set; }
+        public string Email { get;  set; }
+        public DateTime OcurreuEm { get;  set; }
+    }
+}
