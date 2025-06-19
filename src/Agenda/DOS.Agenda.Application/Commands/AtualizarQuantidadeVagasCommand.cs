ef57d976a2bac57e6761c7ee305e2167ef85ddc1
João <GabrielAlvees616@gmail.com>
2025-06-19 15:01:36 -0300

    Criado método AlterarQuantidadeVagas no Domain de Agenda e criado o Command e CommandHandler na camada de Application

---
+++ src/Agenda/DOS.Agenda.Application/Commands/AtualizarQuantidadeVagasCommand.cs
@@ -0,0 +1,10 @@
+﻿using DOS.Core.Mediator.Commands;
+
+namespace DOS.Agenda.Application.Commands
+{
+    public class AtualizarQuantidadeVagasCommand : ICommand<bool>
+    {
+        public Guid AgendaId { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
---
+++ src/Agenda/DOS.Agenda.Application/CommandsHandlers/AtualizarQuantidadeVagasCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using DOS.Agenda.Application.Commands;
+using DOS.Agenda.Domain;
+using DOS.Core.Mediator.Commands;
+
+namespace DOS.Agenda.Application.CommandsHandlers
+{
+    public class AtualizarQuantidadeVagasCommandHandler : ICommandHandler
+        <AtualizarQuantidadeVagasCommand,bool>
+    {
+        private readonly IHorarioRepository _repository;
+
+        public AtualizarQuantidadeVagasCommandHandler(IHorarioRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<bool> HandleAsync(AtualizarQuantidadeVagasCommand command)
+        {
+            var agenda = await _repository.ObterPorIdAsync(command.AgendaId);
+            if (agenda == null) throw new Exception("Agenda não encontrada");
+            agenda.AlterarQuantidadeVagas(command.Quantidade);
+            var sucesso = await _repository.UnitOfWork.Commit();
+            if (!sucesso) throw new Exception("Não foi possível atualizar a quantidade de vagas");
+            return true;
+        }
+    }
+}
