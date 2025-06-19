3997aa3ba3ecb1cfe2675ff562c5ad279a4bc26e
João <GabrielAlvees616@gmail.com>
2025-06-19 12:02:03 -0300

    Criado o command e o commandHandler de Deletar em application do contexto de Agenda

---
+++ src/Agenda/DOS.Agenda.Application/Commands/DeletarAgendaCommand.cs
@@ -0,0 +1,15 @@
+﻿
+using DOS.Core.Mediator.Commands;
+
+namespace DOS.Agenda.Application.Commands
+{
+    public class DeletarAgendaCommand : ICommand<bool>
+    {
+        public Guid AgendaId { get; set; }
+
+        public DeletarAgendaCommand(Guid agendaId)
+        {
+            AgendaId = agendaId;
+        }
+    }
+}
