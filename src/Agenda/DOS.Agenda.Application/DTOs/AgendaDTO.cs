3e35e76cda8af11b898d7bd9057ade046f6f660d
João <GabrielAlvees616@gmail.com>
2025-06-20 20:01:03 -0300

    Adicionado Query de Listar Horários com QueryHandler, DTO e QueryDispatcher

---
+++ src/Agenda/DOS.Agenda.Application/DTOs/AgendaDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace DOS.Agenda.Application.DTOs
+{
+    public class AgendaDTO
+    {
+        public Guid AgendaId { get; set; }
+        public DateTime DataHora { get;  set; }
+
+    }
+}
---
+++ src/Agenda/DOS.Agenda.Application/QueriesHandlers/ListarHorariosQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using DOS.Agenda.Application.DTOs;
+using DOS.Agenda.Application.Queries;
+using DOS.Agenda.Domain;
+using DOS.Core.Mediator.Queries;
+
+namespace DOS.Agenda.Application.QueriesHandlers
+{
+    public class ListarHorariosQueryHandler : IQueryHandler<ListarHorariosQuery, IEnumerable<AgendaDTO>>
+    {
+        private readonly IHorarioRepository _repository;
+
+        public ListarHorariosQueryHandler(IHorarioRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<AgendaDTO>> HandleAsync(ListarHorariosQuery query)
+        {
+            var horarios = await _repository.ObterTodosDisponiveisAsync();
+
+            return horarios.Select(h => new AgendaDTO
+            {
+                AgendaId = h.Id,
+                DataHora = h.DataHora
+          
+            });
+        }
+    }
+}
+        
+
+    
+
