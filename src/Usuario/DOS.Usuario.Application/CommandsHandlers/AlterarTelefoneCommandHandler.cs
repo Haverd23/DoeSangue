64ad1aa2278cd81987f88cb13fc68e9a9acea0e1
João <GabrielAlvees616@gmail.com>
2025-06-23 20:09:52 -0300

    Criado Command e CommandHandler de AlterarTelefone no contexto de Usuario

---
+++ src/Usuario/DOS.Usuario.Application/CommandsHandlers/AlterarTelefoneCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using DOS.Core.Mediator.Commands;
+using DOS.Usuario.Application.Commands;
+using DOS.Usuario.Domain;
+
+namespace DOS.Usuario.Application.CommandsHandlers
+{
+    public class AlterarTelefoneCommandHandler : ICommandHandler
+        <AlterarTelefoneCommand, bool>
+    {
+        private readonly IUsuarioRepository _repository;
+
+        public AlterarTelefoneCommandHandler(IUsuarioRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<bool> HandleAsync(AlterarTelefoneCommand command)
+        {
+
+            var usuario = await _repository.GetById(command.UserId);
+            if (usuario == null) throw new Exception("Usuário não encontrado");
+            usuario.AlterTelefone(command.Telefone);
+            var sucesso = await _repository.UnitOfWork.Commit();
+            if (!sucesso) throw new Exception("Não foi possível alterar o telefone");
+            return true;
+        }
+    }
+}
