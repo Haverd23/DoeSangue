3c68205b792dc064d6de456c72f5ac46b29d5f18
João <GabrielAlvees616@gmail.com>
2025-06-26 14:12:22 -0300

    Criado Exception Global

---
+++ src/Core/DOS.Core/Exceptions/AppException.cs
@@ -0,0 +1,21 @@
+﻿namespace DOS.Core.Exceptions
+{
+    namespace DOS.Core.Exceptions
+    {
+        public class AppException : Exception
+        {
+            public int StatusCode { get; }
+
+            public AppException() { }
+
+            public AppException(string message) : base(message) { }
+
+            public AppException(string message, Exception innerException) : base(message, innerException) { }
+
+            public AppException(string message, int statusCode) : base(message)
+            {
+                StatusCode = statusCode;
+            }
+        }
+    }
+}
